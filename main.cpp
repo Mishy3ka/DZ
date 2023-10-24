80430cb00bbf260c070122a78679e43a8b13d2f9
Mishy3ka <148558959+Mishy3ka@users.noreply.github.com>
2023-10-24 18:58:33 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,22 @@
-//Типы данных переменные константы
-/*Задача: Объявить переменные с помощью которых можно будет сосчитать общую сумму покупкт нескольких товраов. Например плитки школада, кофе и пакеты молока
-*/
-
-
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main(){
-    setlocale(LC_ALL, "RU");
-    int chocolate = 2; // Хранит кол-во упаковок
-    int milk = 3;
-    int coffee = 1;
-    
-    
-    float priceofchocolate = 11.04; // хранит цену за 1 упаковку
-    float priceofmilk = 9.59; 
-    float pricecoffee = 70.77;
-    
-    
-    float sum = 0; //общая сумма
-    
-// считаем стоимость
-
-    sum =(chocolate*priceofchocolate)+(milk*priceofmilk)+(coffee*pricecoffee);
-    cout<<"Общая стоимость покупки = "; //показываем расчет и общую стоимость на экран
-    cout<< chocolate * priceofchocolate <<"+"<< milk*priceofmilk<<"+"<<coffee*pricecoffee;
-    cout<<" = "<< sum << endl<<endl;
-    
-    
-
-  return 0;  
+    float num1;
+    float num2;
+    float resr;
+    float rest;
+    cout<<"Введите первое число: ";
+    cin>>num1;
+    cout<<"Введите второе число: ";
+    cin>>num2;
+    rest=num1*num2;
+    cout<<"Произведение этих чисел: ";
+    cin>>resr;
+    if(resr==rest){
+        cout<<"Все верно";
+    }
+    else{
+        cout<<"Неверно!\n";
+        cout<<"Верный отве:"<<rest;
+    }
 }
